1a477a2caec19b6dc5408f69b181469c7d961fa3
Theo <theo.leffyr@gmail.com>
2019-03-06 16:59:40 +0100

    Add tests for get_script

---
+++ test/test_get_script.py
@@ -0,0 +1,67 @@
+from scripts import get_script
+
+
+def test_get_script_unauthorized():
+    # unauthorized function
+    func = "my_fancy_function"
+    try:
+        _ = get_script(func)
+        assert False
+    except NameError:
+        pass
+
+
+def test_get_script_custom():
+    # custom function
+    row_input = "20191103"
+    func = "clean_date"
+    assert get_script(func)(row_input) == "2019-11-03T00:00:00"
+
+
+def test_get_script_utils():
+    # utility function
+    row_input = " my text  "
+    func = "make_title"
+    assert get_script(func)(row_input) == "My Text"
+
+
+def test_get_script_logic():
+    # logical function
+    func = 'if_valid(clean_identity, "Hello")'
+
+    row_input = ""
+    assert get_script(func)(row_input) == ""
+
+    row_input = "holà"
+    assert get_script(func)(row_input) == "Hello"
+
+    row_input = "NaN"
+    assert get_script(func)(row_input) == ""
+
+    func = "if_valid(clean_identity, 'Hello')"
+    row_input = "holà"
+    assert get_script(func)(row_input) == "Hello"
+
+    func = 'if_valid(clean_identity, "7")'
+    row_input = "holà"
+    assert get_script(func)(row_input) == "7"
+
+    func = 'if_valid(clean_identity, "9.2")'
+    row_input = "holà"
+    assert get_script(func)(row_input) == "9.2"
+
+    func = "if_valid(clean_identity, make_title)"
+
+    row_input = ""
+    assert get_script(func)(row_input) == ""
+
+    row_input = "  holà"
+    assert get_script(func)(row_input) == "Holà"
+
+    # Un authorized param
+    func = "if_valid(clean_identity, my_fancy_function)"
+    try:
+        _ = get_script(func)
+        assert False
+    except ValueError:
+        pass
