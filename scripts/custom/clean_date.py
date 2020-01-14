6039f19eced831553e32765a9eecf941cf677ec4
Jasopaum <jason.paumier@gmail.com>
2020-01-14 11:51:26 +0100

    add setup file to make the package installable with pip

--- scripts/custom/clean_date.py
+++ scripts/custom/clean_date.py
@@ -4,15 +4,16 @@ from scripts import utils
 
 
 def clean_date(raw_input):
+    if not isinstance(raw_input, str):
+        raw_input = str(raw_input)
     if utils.is_empty(raw_input):
         return ""
 
     # Handle YYYYMMDD
     try:
-        date = datetime.datetime.strptime(raw_input, "%Y%m%d")
-        iso_date = date.isoformat()
+        date = datetime.datetime.strptime(raw_input, "%Y-%m-%d %H:%M:%S")
+        iso_date = date.strftime("%Y-%m-%d")
         return iso_date
     except ValueError:
-        pass
+        return raw_input
 
-    return raw_input
---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+requirements = read("requirements.txt").split()
+
+setup(
+    name="cleaning-scripts",
+    version="0.1",
+    description="Python scripts used in the FHIR integration pipeline to clean input data for different external sources.",
+    url="https://github.com/arkhn/cleaning-scripts",
+    author_email="contact@arkhn.org",
+    license="Apache License 2.0",
+    packages=find_packages(exclude=["app*", "test*"]),
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+)
