330fb7b217fbff414b3899cba345f1bc1a3ec8bf
Jasopaum <jason.paumier@gmail.com>
2020-01-14 12:24:46 +0100

    fix tests

--- scripts/custom/clean_date.py
+++ scripts/custom/clean_date.py
@@ -9,11 +9,22 @@ def clean_date(raw_input):
     if utils.is_empty(raw_input):
         return ""
 
+    date = None
+
     # Handle YYYYMMDD
     try:
+        date = datetime.datetime.strptime(raw_input, "%Y%m%d")
+    except ValueError:
+        pass
+
+    # Handle YYYY-MM-DD H:M:S
+    try:
         date = datetime.datetime.strptime(raw_input, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        pass
+
+    if date:
         iso_date = date.strftime("%Y-%m-%d")
         return iso_date
-    except ValueError:
+    else:
         return raw_input
-
--- scripts/utils/select_first_not_empty.py
+++ scripts/utils/select_first_not_empty.py
@@ -4,5 +4,5 @@ from scripts.utils import is_empty
 def select_first_not_empty(*args):
     """Merging script which select the first input not empty"""
     for arg in args:
-        if not is_empty(arg) and arg != "None":
+        if not is_empty(arg):
             return arg
