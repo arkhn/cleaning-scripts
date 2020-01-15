111015ba866e85c9d962f49da5c4b814c3ee0376
Theo <theo.leffyr@gmail.com>
2020-01-15 18:53:05 +0100

    Fix final?

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = read("requirements.txt").split()
 
 setup(
     name="cleaning-scripts",
-    version="0.1.0a1",
+    version="0.1.0a2",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Python scripts used in the FHIR integration pipeline "
