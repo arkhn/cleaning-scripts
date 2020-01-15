011706a277327a7124cb160678db399f54c1fd1c
Théo Ryffel <theo.leffyr@gmail.com>
2020-01-15 19:17:22 +0100

    Update github workflow to publish to pypi & docker hub (#14)
    * Update workflow
    
    * flake8
    
    * Fix
    
    * Fix
    
    * fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Add pypi integration
    
    * Add requirements-dev
    
    * fiix
    
    * fix
    
    * fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix final?
    
    * Fix req

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ requirements = read("requirements.txt").split()
 
 setup(
     name="cleaning-scripts",
-    version="0.1",
-    description="Python scripts used in the FHIR integration pipeline to clean input data for different external sources.",
-    url="https://github.com/arkhn/cleaning-scripts",
+    version="0.1.0a2",
+    author="Arkhn",
     author_email="contact@arkhn.org",
+    description="Python scripts used in the FHIR integration pipeline "
+    "to clean input data for different external sources.",
+    url="https://github.com/arkhn/cleaning-scripts",
     license="Apache License 2.0",
     packages=find_packages(exclude=["app*", "test*"]),
     long_description=read("README.md"),
