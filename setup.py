e0be1c20bcd4f13b0d82e0423cd49dc9149ce780
simonvadee <simon.vadee@gmail.com>
2020-01-27 18:32:37 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = read("requirements.txt").split()
 
 setup(
     name="cleaning-scripts",
-    version="0.1.0a2",
+    version="0.2.0",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Python scripts used in the FHIR integration pipeline "
