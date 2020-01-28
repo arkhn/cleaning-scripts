b858adccc8b7bc2784dbf6ddc166230e804c2082
Jasopaum <jason.paumier@gmail.com>
2020-01-28 09:34:45 +0100

    add manifest (#16)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = read("requirements.txt").split()
 
 setup(
     name="cleaning-scripts",
-    version="0.2.0",
+    version="0.2.3",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Python scripts used in the FHIR integration pipeline "
