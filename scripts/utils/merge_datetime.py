958f56cb58418e5596014f86d6d79a18418dd8b3
simonvadee <simon.vadee@gmail.com>
2020-01-30 10:25:09 +0100

    merge scripts filter out None values

--- scripts/utils/merge_datetime.py
+++ scripts/utils/merge_datetime.py
@@ -1,3 +1,4 @@
 def merge_datetime(*args):
     """Merging script with a datetime concatenation"""
-    return "T".join(args)
+    values = [v for v in args if v is not None]
+    return "T".join(values)
