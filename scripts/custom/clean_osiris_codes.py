325006a53e057399089a1b91a1ba8b5c5a1a657f
Nicolas Riss <nicolas.riss22@gmail.com>
2020-02-14 10:41:30 +0100

    managing empty case for clean_osiris_codes, map_gender_osiris and map_genetic_permission

--- scripts/custom/clean_osiris_codes.py
+++ scripts/custom/clean_osiris_codes.py
@@ -1,9 +1,11 @@
+from scripts import utils
+
 # function clean_osiris_codes
 # remove all chars before and including the first colon
 
 
 def clean_osiris_codes(raw_input):
-    if raw_input is None:
+    if utils.is_empty(raw_input):
         return None
     firstColonIndex = raw_input.find(":") + 1
     return raw_input[firstColonIndex:]
--- scripts/custom/map_genetic_permission_osiris.py
+++ scripts/custom/map_genetic_permission_osiris.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from scripts import utils
 
 
 class Authorization(Enum):
@@ -7,6 +8,8 @@ class Authorization(Enum):
 
 
 def map_genetic_permission_osiris(raw_input):
+    if utils.is_empty(raw_input):
+        return None
     mapping = {
         "UMLS:C1298907": Authorization.PERMIT.value,
         "UMLS:C1298908": Authorization.DENY.value,
