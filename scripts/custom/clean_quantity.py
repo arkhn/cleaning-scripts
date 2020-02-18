e1d5fe04a309b35520d033287cc5542dfa89129d
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-02-18 14:07:36 +0100

    add script to clean quantity

---
+++ scripts/custom/clean_quantity.py
@@ -0,0 +1,13 @@
+from scripts import utils
+import re
+def clean_quantity(raw_input):
+    """Removes that input is conform to FHIR quantity type
+    """
+    
+    if utils.is_empty(raw_input):
+        return ""
+    number = re.search(r"-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][+-]?[0-9]+)?", raw_input)
+    if not number or number.group(0)!=raw_input:
+        return ""
+    else:
+        return raw_input
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = read("requirements.txt").split()
 
 setup(
     name="cleaning-scripts",
-    version="0.2.9",
+    version="0.2.10",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Python scripts used in the FHIR integration pipeline "
