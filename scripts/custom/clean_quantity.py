2dda4b71378c5a7674ee44a4381d004d817dc80a
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-02-18 15:10:28 +0100

    add logging

--- scripts/custom/clean_quantity.py
+++ scripts/custom/clean_quantity.py
@@ -1,5 +1,6 @@
 from scripts import utils
 import re
+import logging
 
 
 def clean_quantity(raw_input):
@@ -10,6 +11,11 @@ def clean_quantity(raw_input):
         return ""
     number = re.search(r"-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][+-]?[0-9]+)?", raw_input)
     if not number or number.group(0) != raw_input:
+        logging.warning(
+            "The quantity cleaning-script has removed input {}, not of decimal type".format(
+                raw_input
+            )
+        )
         return ""
     else:
         return float(raw_input)
