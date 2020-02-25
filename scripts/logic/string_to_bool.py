cac5666026a6bd7f819f7e93d2ae3de9f63a7a3d
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-02-25 13:43:58 +0100

    format

--- scripts/logic/string_to_bool.py
+++ scripts/logic/string_to_bool.py
@@ -1,8 +1,13 @@
 def string_to_bool(raw_input):
     """Convert ("True","true","TRUE") to (True).."""
-    mapping = {"True": True, "False": False, 
-               "true": True, "false": False, 
-               "TRUE": True, "FALSE": False}
+    mapping = {
+        "True": True,
+        "False": False,
+        "true": True,
+        "false": False,
+        "TRUE": True,
+        "FALSE": False,
+    }
     if raw_input in mapping.keys():
         return mapping[raw_input]
     else:
