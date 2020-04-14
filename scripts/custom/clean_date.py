77cea4fcec994c4202b5b4de770a195c2112631b
Jasopaum <jason.paumier@gmail.com>
2020-04-14 14:54:14 +0200

    flake

--- scripts/custom/clean_date.py
+++ scripts/custom/clean_date.py
@@ -18,20 +18,18 @@ def clean_date(raw_input):
         pass
 
     # Handle YYYY-MM-DD H:M:S
-    if date is None:
-        try:
-            date = datetime.datetime.strptime(raw_input, "%Y-%m-%d %H:%M:%S")
-        except ValueError:
-            pass
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        pass
 
     # Handle YYYY-MM-DDTH:M:S
-    if date is None:
-        try:
-            date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S")
-        except ValueError:
-            pass
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S")
+    except ValueError:
+        pass
 
-    if date is not None:
-        return date.strftime("%Y-%m-%d")
-    else:
+    if date is None:
         return raw_input
+
+    return date.strftime("%Y-%m-%d")
