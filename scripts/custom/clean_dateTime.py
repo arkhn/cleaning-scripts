7fcb8d6764231fd2464a496e087f4b4deec371f9
ilan_zana <ilan.zana26@gmail.com>
2020-06-02 15:42:59 +0200

    clean_dateTime format script

---
+++ scripts/custom/clean_dateTime.py
@@ -0,0 +1,47 @@
+import datetime
+
+from scripts import utils
+
+
+def clean_dateTime(raw_input):  # noqa: C901
+    if not isinstance(raw_input, str):
+        raw_input = str(raw_input)
+    if utils.is_empty(raw_input):
+        return ""
+
+    date = None
+
+    # Handle YYYY
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y")
+    except ValueError:
+        pass
+
+    # Handle YYYYMM
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y%m")
+    except ValueError:
+        pass
+
+    # Handle YYYYMMDD
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y%m%d")
+    except ValueError:
+        pass
+
+    # Handle YYYY-MM-DDTH:M:S
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S")
+    except ValueError:
+        pass
+
+    # Handle RFC 1123 format
+    try:
+        date = datetime.datetime.strptime(raw_input, "%a, %d %b %Y %H:%M:%S GMT")
+    except ValueError:
+        pass
+
+    if date is None:
+        return raw_input
+
+    return date.strftime("%Y-%m-%dT%H:%M:%SZ")
