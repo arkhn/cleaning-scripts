88c80920d6041ee9e07b36cf58045bc6acffcc2e
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-07-16 15:40:50 +0200

    add lint

--- scripts/custom/clean_date.py
+++ scripts/custom/clean_date.py
@@ -10,7 +10,7 @@ def clean_date(raw_input):  # noqa: C901
         return ""
 
     date = None
-    
+
     # Handle YYYY
     try:
         date = datetime.datetime.strptime(raw_input, "%Y")
@@ -52,13 +52,12 @@ def clean_date(raw_input):  # noqa: C901
         date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S")
     except ValueError:
         pass
-    
+
     # Handle YYYY-MM-DDTH:M:S+zzzz
     try:
         date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S%z")
     except ValueError:
         pass
-
 
     # Handle RFC 1123 format
     try:
@@ -71,14 +70,17 @@ def clean_date(raw_input):  # noqa: C901
         date = datetime.datetime.strptime(raw_input, "%Y%m%d%H%M")
     except ValueError:
         pass
-    
+
     # Correct format
     try:
-        full_match = re.fullmatch(r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?", raw_input)
+        full_match = re.fullmatch(
+            r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?",
+            raw_input,
+        )
         date = datetime.datetime.strptime(full_match.group(0)[0:10], "%Y-%m-%d")
     except Exception:
         pass
-    
+
     if date is None:
         return raw_input
 
