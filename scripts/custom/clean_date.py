cd836fe42828b8eecd24c01b85f505d9b8681158
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-07-16 15:38:21 +0200

    fix tests

--- scripts/custom/clean_date.py
+++ scripts/custom/clean_date.py
@@ -1,5 +1,5 @@
 import datetime
-
+import re
 from scripts import utils
 
 
@@ -10,6 +10,24 @@ def clean_date(raw_input):  # noqa: C901
         return ""
 
     date = None
+    
+    # Handle YYYY
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y")
+    except ValueError:
+        pass
+
+    # Handle YYYY-MM
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y-%m")
+    except ValueError:
+        pass
+
+    # Handle YYYYMM
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y%m")
+    except ValueError:
+        pass
 
     # Handle YYYY-MM-DD
     try:
@@ -34,10 +52,17 @@ def clean_date(raw_input):  # noqa: C901
         date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S")
     except ValueError:
         pass
+    
+    # Handle YYYY-MM-DDTH:M:S+zzzz
+    try:
+        date = datetime.datetime.strptime(raw_input, "%Y-%m-%dT%H:%M:%S%z")
+    except ValueError:
+        pass
+
 
     # Handle RFC 1123 format
     try:
-        date = datetime.datetime.strptime(raw_input, "%a, %d %b %Y %H:%M:%S GMT")
+        date = datetime.datetime.strptime(raw_input, "%a, %d %b %Y %H:%M:%S %Z")
     except ValueError:
         pass
 
@@ -46,7 +71,14 @@ def clean_date(raw_input):  # noqa: C901
         date = datetime.datetime.strptime(raw_input, "%Y%m%d%H%M")
     except ValueError:
         pass
-
+    
+    # Correct format
+    try:
+        full_match = re.fullmatch(r"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?", raw_input)
+        date = datetime.datetime.strptime(full_match.group(0)[0:10], "%Y-%m-%d")
+    except Exception:
+        pass
+    
     if date is None:
         return raw_input
 
