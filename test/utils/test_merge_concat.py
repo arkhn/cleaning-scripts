ea513a728a9de0b65e6f65bb8ba637860f8881cb
nriss <nicolas.riss22@gmail.com>
2020-10-28 12:33:05 +0100

    adding date type and boolean testing for merge concat scripts

--- test/utils/test_merge_concat.py
+++ test/utils/test_merge_concat.py
@@ -1,15 +1,37 @@
 from scripts import utils
+import datetime
 
 
 def test_merge_concat():
+
+    # Tests string
+
     assert utils.merge_concat("1") == "1"
 
     assert utils.merge_concat("1", "2") == "1 2"
 
-    assert utils.merge_concat(1, 2) == "1 2"
-
     assert utils.merge_concat("1", "2", "3") == "1 2 3"
+
+    assert utils.merge_concat("a", "b", "ab") == "a b ab"
+
+    # Tests integer
+
+    assert utils.merge_concat(1, 2) == "1 2"
 
     assert utils.merge_concat(1, 2, 3) == "1 2 3"
 
-    assert utils.merge_concat("a", "b", "ab") == "a b ab"
+    # Tests date
+
+    dateNow = datetime.datetime.now()
+    assert utils.merge_concat("a", dateNow) == "a " + str(dateNow)
+
+    assert (
+        utils.merge_concat("testing", datetime.datetime(2020, 5, 17))
+        == "testing 2020-05-17 00:00:00"
+    )
+
+    # Tests boolean
+
+    assert utils.merge_concat(True) == "True"
+
+    assert utils.merge_concat(True, False) == "True False"
--- test/utils/test_merge_concat_attached.py
+++ test/utils/test_merge_concat_attached.py
@@ -1,16 +1,16 @@
 from scripts import utils
+import datetime
 
 
 def test_merge_concat_attached():
+
+    # Tests string
+
     assert utils.merge_concat_attached("1") == "1"
 
     assert utils.merge_concat_attached("1", "2") == "12"
 
-    assert utils.merge_concat_attached(1, 2) == "12"
-
     assert utils.merge_concat_attached("1", "2", "3") == "123"
-
-    assert utils.merge_concat_attached(1, 2, 3) == "123"
 
     assert utils.merge_concat_attached("a", "b", "ab") == "abab"
 
@@ -20,3 +20,25 @@ def test_merge_concat_attached():
         utils.merge_concat_attached("Pyrog      ", " Is         ", "  Awesome ")
         == "Pyrog       Is           Awesome "
     )
+
+    # Tests integer
+
+    assert utils.merge_concat_attached(1, 2) == "12"
+
+    assert utils.merge_concat_attached(1, 2, 3) == "123"
+
+    # Tests date
+
+    dateNow = datetime.datetime.now()
+    assert utils.merge_concat_attached("a", dateNow) == "a" + str(dateNow)
+
+    assert (
+        utils.merge_concat_attached("testing", datetime.datetime(2020, 5, 17))
+        == "testing2020-05-17 00:00:00"
+    )
+
+    # Tests boolean
+
+    assert utils.merge_concat_attached(True) == "True"
+
+    assert utils.merge_concat_attached(True, False) == "TrueFalse"
