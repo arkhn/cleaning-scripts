7a31248da89fb27144398640625aabd0cc613d08
nriss <nicolas.riss22@gmail.com>
2020-10-28 15:46:50 +0100

    renaming and improving script comments

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,7 +1,7 @@
 """Utility scripts"""
 from .is_empty import is_empty
 from .make_title import make_title
-from .merge_concat_attached import merge_concat_attached
+from .concat_without_separator import concat_without_separator
 from .merge_concat import merge_concat
 from .merge_datetime import merge_datetime
 from .select_first_not_empty import select_first_not_empty
@@ -14,7 +14,7 @@ __all__ = [
     "is_empty",
     "make_title",
     "merge_datetime",
-    "merge_concat_attached",
+    "concat_without_separator",
     "merge_concat",
     "select_first_not_empty",
     "strip",
--- scripts/utils/merge_concat.py
+++ scripts/utils/merge_concat.py
@@ -1,5 +1,5 @@
 def merge_concat(*args):
-    """Merging script with a simple concatenation, space-between"""
+    """Merging script with a simple concatenation and a " " separator"""
     values = [str(v) for v in args if v is not None]
     separator = " "
     return separator.join(values)
