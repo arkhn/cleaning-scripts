75a8a529d6a6c89bdff2d7d570b8df6c9cd841fb
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-11-16 16:55:18 +0100

    change strings to numbers to adapt to change in river

--- scripts/custom/map_2_true.py
+++ scripts/custom/map_2_true.py
@@ -4,7 +4,7 @@ from scripts import utils
 def map_2_true(raw_input):
     """Map some code from (2->True) and None otherwise
     """
-    if utils.is_empty(raw_input) or raw_input != "2":
+    if utils.is_empty(raw_input) or raw_input != 2:
         return None
     else:
         return True
