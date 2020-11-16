eb5adaf9a81d76a681da41b0b5a9b2d5adb40eb2
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-11-16 17:39:52 +0100

    fix scripts, rebasing on master

--- scripts/logic/binary_to_bool_1.py
+++ scripts/logic/binary_to_bool_1.py
@@ -1,6 +1,6 @@
 def binary_to_bool_1(raw_input):
     """Map (0,1) to (False, True)"""
-    mapping = {"0": False, "1": True}
+    mapping = {0: False, 1: True}
     if raw_input in mapping.keys():
         return mapping[raw_input]
     else:
