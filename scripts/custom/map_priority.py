6a766746ecb9a1a514427e4d2950964645128c27
nriss <nicolas.riss22@gmail.com>
2021-05-27 17:38:01 +0200

    cast to string

--- scripts/custom/map_priority.py
+++ scripts/custom/map_priority.py
@@ -11,12 +11,12 @@ class Priority(Enum):
 def map_priority(raw_input):
     """Map int to ServiceRequest.priority (0: stat, 1: asap, 2: urgent, 3+ routine)"""
     mapping = {
-        0: Priority.STAT.value,
-        1: Priority.ASAP.value,
-        2: Priority.URGENT.value,
-        3: Priority.ROUTINE.value,
+        "0": Priority.STAT.value,
+        "1": Priority.ASAP.value,
+        "2": Priority.URGENT.value,
+        "3": Priority.ROUTINE.value,
     }
-    if raw_input in mapping.keys():
-        return mapping[raw_input]
+    if str(raw_input) in mapping.keys():
+        return mapping[str(raw_input)]
     else:
         return mapping[3]
